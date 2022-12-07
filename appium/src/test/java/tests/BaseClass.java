b70f040878ac41713ba628b97ec3171016b37a6b
nam.nguyen <namnguyen.pro@live.com>
2022-12-07 09:41:24 +0700

    init tests folder - create Before and After function
    create BaseTest class
    init BeforeTest
    init AfterTest

---
+++ appium/src/test/java/tests/BaseClass.java
@@ -0,0 +1,16 @@
+package tests;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class BaseClass {
+    @BeforeTest
+    public void setup() {
+        
+    }
+
+    @AfterTest
+    public void tearDown() {
+        
+    }
+}
